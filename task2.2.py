b9e9c72b55c58080e147fbd29c43d3e54e4f8e89
Poci01 <luthfimaulana1994@gmail.com>
2026-09-15 10:10:40 +0700

    Added new project

---
+++ task2.2.py
@@ -0,0 +1,24 @@
+import string
+import random
+
+def pw_maker(long=10, angka=True, simbol=True):
+    karakter = string.ascii_letters
+
+    if angka:
+        karakter += string.digits
+
+    if simbol:
+        karakter += string.punctuation
+
+
+    pw = "".join(random.choices(karakter, k=long))
+
+    return pw
+
+print("Default (Huruf + Angka): ", pw_maker())
+``
+print("Hanya Huruf: ", pw_maker(angka=False))
+
+print("Lengkap (Huruf + Angka + Simbol): ", pw_maker(simbol=True))
+
+print("Custom 15 Karakter: ", pw_maker(long=15, simbol=True))
